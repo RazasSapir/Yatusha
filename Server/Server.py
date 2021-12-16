2c8f1f379718b9317670cd9d15aefdabdc413ba9
t8875881 <razsap6@gmail.com>
2021-12-16 17:46:24 +0200

    Added requirements.txt

--- Server/Server.py
+++ Server/Server.py
@@ -2,14 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+IP = "0.0.0.0"
+PORT = 80
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Yatusha is on the Web!</p>"
+    return "<p>Yatusha is Here!</p>"
 
 
 def main():
-    app.run()
+    app.run(host=IP, port=PORT)
 
 
 if __name__ == '__main__':
