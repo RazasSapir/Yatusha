ad9bba56d5f16409e5613a73b4670b8127767806
RawJeff <roy151500@gmail.com>
2021-12-22 17:11:55 +0200

    Added post fetch method

---
+++ src/FetchNPost.js
@@ -0,0 +1,10 @@
+function Post(data){
+    fetch('localhost/echo', {
+  method: "POST",
+  body: JSON.stringify(_data),
+  headers: {"Content-type": "application/json; charset=UTF-8"}
+})
+.then(response => response.json()) 
+.then(json => console.log(json))
+.catch(err => console.log(err));
+}
