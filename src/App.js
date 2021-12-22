2aab2c1f5e8472967f8ba8f6445a36ce8ed5bb1a
ariel <ariel.sheffer@mail.huji.ac.il>
2021-12-22 17:02:24 +0200

    app is a class now

--- src/App.js
+++ src/App.js
@@ -19,22 +19,79 @@ class RedirctLink extends React.Component{
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component{
 
-        <form>
-          <label for="fname">First name:</label>
-          <input type="text" id="fname" name="fname"/><br/><br/>
-          <label for="lname">Last name:</label>
-          <input type="text" id="lname" name="lname"/><br/><br/>
-          <input type="submit" value="Submit"/>
-        </form>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+     this.state={
+       first_name: "",
+       last_name: "",
+       list:[]
+     }
+  }
+
+  updateName(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    });
+    console.log("owo"); // for testing
+  }
+
+  addItem(){
+    // create item with unic id
+      const newItem={
+      id: 1+Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy the current list
+    const list = [...this.state.list];
+
+    //add new item
+    list.push(newItem);
+
+    //update state
+    this.setState({
+      list,
+      first_name: "",
+      last_name: ""
+    });
+
+    console.log(list); //for testing
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+          <form>
+            <label htmlFor="fname">First name:</label>
+            <input type="text" id="fname" name="fname"
+            onChange={e=> this.updateName("first_name", e.target.value)}/>
+            <br/><br/>
+            <label htmlFor="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"
+            onChange={e=> this.updateName("last_name", e.target.value)}/>
+            <br/><br/>
+            <input type="submit" value="Submit"/>
+            <ul>
+              {this.state.list.map(item=>{
+                return(
+                  <li key={item.id}>
+                    {item.value}
+                  </li>
+                )
+              })}
+            </ul>
+          </form>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 // Routing between the pages
