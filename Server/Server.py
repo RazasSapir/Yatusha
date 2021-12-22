29eba4c84c038c0f38cccaf77cf66d397b5e58ff
Noam Broza <noamb@localhost.localdomain>
2021-12-22 17:04:10 +0200

    added server.py

--- Server/Server.py
+++ Server/Server.py
@@ -1,9 +1,10 @@
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify, abort, abort
 import requests
 from pymongo import MongoClient
 from secret_keys.Private_URIs import MONGO_URI
 import flask_pymongo
 import mongoengine
+import json
 
 # Globals
 app = Flask(__name__)
@@ -30,16 +31,31 @@ def echo_page():
 
 @app.route("/test_json")
 def test_json():
-    res = requests.post('http://localhost:80/echo', json={"mytext": "lalala"})
+    res = requests.post('http://localhost:80/save', json={"name": 'lalala', "license_type" : '2',\
+     "location" : {"type": "Point", "coordinates": [81.4471435546875, 23.61432859499169]},\
+        "place_type": "Public Space", "pest_type": "cricket", "pesticides_ID" : "1234",\
+            "additional_information": "abc"})
     if res.ok:
         return "Worked" + res.text
-    return res.ok
+    return str(res.ok)
 
 
 @app.route("/")
 def hello_world():
     return send_from_directory("../assets", "Test.html")
 
+@app.route("/save", methods=['POST'])
+def save_db():
+    content = request.json
+    data = json.loads(str(content))
+    try:
+        add_pesticing_to_db = PesticingToDB(data["name"], data["license_type"], data["location"], \
+            data["place_type"], data["pest_type"], data["pesticides_ID"], data["additional_information"])
+    except ValueError:
+        abort(400, description="invalid place type")
+    add_pesticing_to_db.save()
+    return jsonify(content)
+    
 
 def main():
     if ACTIVATE_SSL:
