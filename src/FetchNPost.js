b11d2030ce4579d79bc6fe8d0b122392169e368e
RawJeff <roy151500@gmail.com>
2021-12-28 14:34:37 +0200

    added some fixes

--- src/FetchNPost.js
+++ src/FetchNPost.js
@@ -1,11 +1,20 @@
-function Post(data){
+export default function Post(data){
+  console.log("wow")
     console.log(JSON.stringify(data));
-    fetch('localhost/echo', {
-  method: "POST",
-  body: JSON.stringify(data),
-  headers: {"Content-type": "application/json; charset=UTF-8"}
-})
-.then(response => response.json()) 
+    console.log("wow")
+    fetch('http://localhost/echo', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+      // "Origin": "http:/localhost:3000",
+      // "Access-Control-Request-Method": "POST",
+      // "Access-Control-Request-Headers": {"Content-type": "application/json; charset=UTF-8"}
+  })
+.then(response => {
+  console.log("owo");
+  if(response.ok){return response.json();}
+  throw response;
+}) 
 .then(json => console.log(json))
 .catch(err => console.log(err));
 }
