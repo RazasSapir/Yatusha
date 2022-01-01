f8678d209346569a4ad6d559839d90b76fdf4849
RawJeff <roy151500@gmail.com>
2022-01-02 01:19:38 +0200

    added redirection for buttons

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,20 @@
 import React from "react"
 import yatushaLogo from "../images/logoYatusha.jfif"
 import "../style/HomePage.css"
+import { useNavigate } from "react-router-dom"
 
 export default function HomePage(){
     const about = "לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק. קולהע צופעט למרקוח איבן איף, ברומץ כלרשט מיחוצים. קלאצי קולהע צופעט למרקוח איבן איף, ברומץ כלרשט מיחוצים. קלאצי גולר מונפרר סוברט לורם שבצק יהול, לכנוץ בעריר גק ליץ, גולר מונפרר סוברט לורם שבצק יהול, לכנוץ בעריר גק ליץ, ושבעגט ליבם סולגק. בראיט ולחת צורק מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג ישבעס."
+    let navigate = useNavigate();
     return(
-        <>
             <div className="mainPage">
                 <img className ="imgYatusha" src = {yatushaLogo} alt = ""/>
                 <nav className = "navbar">
                     <p>דף 2</p>
-                    <p>דף 1</p>
+                    <p onClick={() => navigate("/form")}>דף 1</p>
                     <p>דף 3</p>
                 </nav>
                 <p className="about">{about}</p>
-                <footer className="main_footer">
-                <p> All rights reserved</p> 
-                <a>ico1</a>
-                <a>ico2</a>
-                <a>ico3</a>
-            </footer>
             </div>
-        </>
     )
 }
